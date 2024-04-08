a2213e4c082b68e5446c2bc6ea5f3d28e05b4905
Marin Aussant <118370229+MarinAussant@users.noreply.github.com>
2024-04-08 17:17:08 +0200

    Update

--- Assets/Scripts/PlayerScript/PlayerStates/inMovementState.cs
+++ Assets/Scripts/PlayerScript/PlayerStates/inMovementState.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class inMovementState : PlayerState
 {
-    
-    
+
+    public override void enterState()
+    {
+        
+    }
+
+    public override void updateState()
+    {
+        
+    }
+
+    public override void exitState()
+    {
+        
+    }
 
 }
